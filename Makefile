85b5ea4386be330201ffd6abfe87d0a4f8f7a65c
matiasSuksi <suksimatias@gmail.com>
2023-09-29 00:56:32 +0300

    First commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+address_book: main.c
+	gcc -std=c99 main.c -o address_book
---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int menu_selection;
+
+typedef struct persons
+{
+  char *p_name;
+  char *p_phone;
+  char *p_email;
+} person;
+
+//Display menu
+
+//Menu
+
+//Add contact
+
+//Search contact
+
+//Edit contact
+
+//Delete contact
+
+//List all contacts
+
+int main(void)
+{
+  printf("Address book\n");
+  return 0;
+}
+
